af687c4ecfec69266f5931e3d3b6557eef5a3c05
armoag <armoag@gmail.com>
2018-09-15 13:02:58 -0700

    fixed bug in returns page

--- ViewModel/ReturnsViewModel.cs
+++ ViewModel/ReturnsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Seiya.WpfBindingUtilities;
@@ -27,6 +29,7 @@ namespace Seiya
         #region Constructors
         public ReturnsViewModel()
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-MX");
             ReturnID = CreateReturnID();
             PurchaseDate = DateTime.Today;
         }
@@ -137,7 +140,7 @@ namespace Seiya
 
         internal bool CanExecute_ReturnSaveChangesCommand(object parameter)
         {
-            return true;
+            return TicketNumber != 0 && ReturnReason != null & CustomerName != null;
         }
         #endregion
 
