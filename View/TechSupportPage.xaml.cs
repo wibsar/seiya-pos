05bd944b3a783e98e0edcff8f31d80797700fe1a
armoag <armoag@gmail.com>
2018-09-22 19:37:42 -0700

    removed .tmp file from Resources and fixed double declaration of event at loginpage

--- View/TechSupportPage.xaml.cs
+++ View/TechSupportPage.xaml.cs
@@ -24,12 +24,6 @@ namespace Seiya
             DataContext = MainWindowViewModel.GetInstance();
         }
 
-        private void OnNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-            e.Handled = true;
-        }
-
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
